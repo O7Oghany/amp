6db32eb65278b983b5c87c28ae54142f05e44f2b
Muhammed Abdelmontalib <abdelmmu@gmail.com>
2021-01-14 21:51:35 +0100

    ading new README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# amp
-this will contain SDK to interact with amp API
+# AMP
+#### **SDK** to interact with *AMP* API 
+__________________________________________________
+
+for more infomrations about the _AMP_ _API_
+> check [AMP API Docs] (https://api-docs.amp.cisco.com/api_resources?api_host=api.eu.amp.cisco.com&api_version=v1).
--- base/amp.go
+++ base/amp.go
@@ -1,22 +1,25 @@
 package base
 
 import (
-	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"net/http"
 	"regexp"
+
+	"github.com/sirupsen/logrus"
 )
 
-
+//AMP TODO NOTE
 type AMP struct {
-	BaseURL string
+	BaseURL  string
 	ClientID string
-	APIKey string
-	Client    Client
+	APIKey   string
+	Client   CustomClient
 }
+
 //CustomClient provides the interface for custom Do request
 //Do not need to custom the whole http.Client, just Do to add basic Auth
-type Client interface {
-	Do(req *http.Request)(*http.Response, error)
+type CustomClient interface {
+	Do(req *http.Request) (*http.Response, error)
 }
 
 //AMPBaseURL will expose the base URL
@@ -26,18 +29,18 @@ const AMPBaseURL = "https://api.eu.amp.cisco.com/v1"
 
 //validResource contain the regular expression for the valid resource
 //resource must begin with "/"
-const validResource =`\/[-a-zA-Z0-9@:%_\+.~#?&//=]*`
+const validResource = `\/[-a-zA-Z0-9@:%_\+.~#?&//=]*`
 
 //NewAMP return the construct for  AMP.
 //Take clientID and apiKey as arguments
 // this SDK support v1
 //
 // more info can be found: https://api-docs.amp.cisco.com/api_resources?api_host=api.eu.amp.cisco.com&api_version=v1
-func NewAMP( clientID, apiKey string) *AMP {
+func NewAMP(clientID, apiKey string) *AMP {
 	return &AMP{
-		BaseURL: AMPBaseURL,
+		BaseURL:  AMPBaseURL,
 		ClientID: clientID,
-		APIKey: apiKey,
+		APIKey:   apiKey,
 	}
 }
 
@@ -47,12 +50,12 @@ func NewAMP( clientID, apiKey string) *AMP {
 //
 //
 //Do will implement basic auth as well as the required headers for the API calls
-func(a *AMP) Do (r *http.Request)(*http.Response, error){
-	baseURL :=  *r.URL
-	newreq, err := http.NewRequest(r.Method,baseURL.String(),r.Body)
-	if err != nil{
+func (a *AMP) Do(r *http.Request) (*http.Response, error) {
+	baseURL := *r.URL
+	newreq, err := http.NewRequest(r.Method, baseURL.String(), r.Body)
+	if err != nil {
 		logrus.WithFields(logrus.Fields{
-			"Error":err,
+			"Error": err,
 		}).Error("could not invoke the http request check the Error Message")
 		return nil, err
 	}
@@ -67,32 +70,45 @@ func(a *AMP) Do (r *http.Request)(*http.Response, error){
 	}
 	return res, nil
 }
+
 //GenericReq acts as generic HTTP reqquest method
 //simplify the Do Method and checking for errors
-func(a *AMP) GenericReq(res, method string)(http.ResponseWriter, error){
+func (a *AMP) GenericReq(res, method string) ([]byte, error) {
 	url := a.BaseURL + res
 	var resource = regexp.MustCompile(validResource)
-	if !resource.MatchString(res){
+	if !resource.MatchString(res) {
 		logrus.WithFields(logrus.Fields{
 			"Error": "not correct resource patter.",
-			"Info:":"make sure that you have '/' for the resource. check https://api-docs.amp.cisco.com/api_resources?api_host=api.eu.amp.cisco.com&api_version=v1",
+			"Info:": "make sure that you have '/' for the resource. check https://api-docs.amp.cisco.com/api_resources?api_host=api.eu.amp.cisco.com&api_version=v1",
 		}).Errorf("please check the resources. Check Info for more information")
 	}
-	req, err := http.NewRequest(method,url,nil)
-	if err != nil{
-		logrus.WithFields(logrus.Fields{
-			"Error":err,
-			"Url": url,
-		}).Errorf("could not invoke the http request check the Error Message")
-		return nil, err
-	}
-	res,err := a.Client.Do(req)
+	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
 			"Error": err,
+			"Url":   url,
+		}).Errorf("could not invoke the http request check the Error Message")
+		return nil, err
+	}
+	result, err := a.Client.Do(req)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"Error":     err,
+			"ErrorCode": result.StatusCode,
 		}).Error("could not invoke the http request check the Error Message")
 		return nil, err
 	}
+	defer result.Body.Close()
 
+	b, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"Error":        err,
+			"ErrorCode":    result.StatusCode,
+			"ErrorMessage": "", //TODO
+		}).Error("could not read the http response, check the Error Message")
+		return nil, err
+	}
+	return b, nil
 
 }
